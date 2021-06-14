6a0fac9c7dcd3ba87a38ee8cb3229411a96c361d
Arpit <arpitkhatri5@gmail.com>
2021-06-14 16:11:11 +0530

    minor edit

--- migration/requestUrl.js
+++ migration/requestUrl.js
@@ -1,8 +1,8 @@
 const request_url = require('../models/requestUrl');
 
-const insertRequestData = (short_urlValue, booleanResult, ipAddress) => {
+const insertRequestData = (shortUrlValue, booleanResult, ipAddress) => {
   return new request_url({
-    short_url: short_urlValue,
+    short_url: shortUrlValue,
     result: booleanResult,
     created_at: new Date(),
     ip: String(ipAddress),
